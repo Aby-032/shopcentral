74287bbb9250993c6bcc0f81c40d476d2221e282
Aby Joseph <abyjoseph.032@gmail.com>
2026-04-08 12:01:21 +0530

    Added Login and Logout

--- shopcentral/users/urls.py
+++ shopcentral/users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import register_view, user_login, user_logout
 
 urlpatterns = [
-    
+    path('register/', register_view, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
 ]
